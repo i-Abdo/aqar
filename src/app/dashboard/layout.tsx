702bcaa3f39065dd61ac9f0ba4657748eae2d0eb
abdo kh <abdo0050607@gmail.com>
2025-06-17 21:35:27 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <DashboardNav>
    
    wi

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -5,7 +5,6 @@ import { useRouter } from "next/navigation";
 import React, { useEffect } from "react";
 import { DashboardNav } from "@/components/dashboard/DashboardNav";
 import { Loader2 } from "lucide-react";
-// import { ScrollArea } from "@/components/ui/scroll-area"; // Removed ScrollArea
 
 export default function DashboardLayout({
   children,
@@ -35,13 +34,12 @@ export default function DashboardLayout({
 
   return (
     <div className="container mx-auto">
-        <div className="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0 rtl:lg:space-x-reverse">
-            <aside className="lg:w-1/5">
+        <div className="flex flex-col lg:flex-row lg:space-x-12 lg:space-y-0 rtl:lg:space-x-reverse">
+            <aside className="lg:w-1/5 h-full lg:sticky lg:top-16 lg:h-[calc(100vh-4rem)] lg:overflow-y-auto lg:border-r rtl:lg:border-l rtl:lg:border-r-transparent border-border py-6 lg:py-0">
                  <DashboardNav />
             </aside>
             <div className="flex-1 lg:max-w-4xl xl:max-w-5xl">
-                {/* Removed ScrollArea wrapper */}
-                <div className="p-1">{children}</div>
+                <div className="p-1 pt-6 lg:pt-1">{children}</div>
             </div>
         </div>
     </div>
