d5be8735c312f7f109c5702b71d5b8094bfc1188
abdo kh <abdo0050607@gmail.com>
2025-06-18 14:42:11 +0000

    https://res.cloudinary.com/dgz2rwp09/image/upload/v1750257678/logo-aqari

--- src/components/layout/AppLogo.tsx
+++ src/components/layout/AppLogo.tsx
@@ -1,15 +1,17 @@
+
 import Link from 'next/link';
 import Image from 'next/image';
 
 export function AppLogo() {
   return (
     <Link href="/" className="flex items-center">
-      <Image 
-        src="/logo-aqari.png" 
-        alt="شعار عقاري" 
+      <Image
+        src="https://res.cloudinary.com/dgz2rwp09/image/upload/v1750257678/logo-aqari_yb470x.png"
+        alt="شعار عقاري"
         width={1024} // Original width for aspect ratio calculation
         height={1024} // Original height for aspect ratio calculation
         className="h-10 w-auto" // CSS to control displayed size
+        priority // Good to add for LCP elements like logos
       />
     </Link>
   );
