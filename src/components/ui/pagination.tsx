4ca6ba765977465132b8e7a49ba5066a69b7ea1d
abdo kh <abdo0050607@gmail.com>
2025-06-21 10:13:32 +0000

    Make the following changes:
    ,
    
    Find this element:
    
    Relevant files:

--- src/components/ui/pagination.tsx
+++ src/components/ui/pagination.tsx
@@ -21,7 +21,7 @@ const PaginationContent = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <ul
     ref={ref}
-    className={cn("flex flex-row items-center gap-1 dir-rtl", className)}
+    className={cn("flex flex-row items-center gap-1", className)}
     {...props}
   />
 ))
