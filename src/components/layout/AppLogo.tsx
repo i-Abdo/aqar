28c0efca7c9430411f165aedde0d763ade7e10b7
abdo kh <abdo0050607@gmail.com>
2025-06-24 10:03:23 +0000

    غير لوقو الموقع الحالي بهذا https://res.cloudinary.com/dgz2rwp09/image/

--- src/components/layout/AppLogo.tsx
+++ src/components/layout/AppLogo.tsx
@@ -7,8 +7,8 @@ export function AppLogo() {
   return (
     <Link href="/" className="flex items-center p-2 -m-2 rounded-lg focus:outline-none focus-visible:ring-2 focus-visible:ring-ring">
       <Image
-        src="https://res.cloudinary.com/dgz2rwp09/image/upload/v1750257678/logo-aqari_yb470x.png"
-        alt="شعار عقاري"
+        src="https://res.cloudinary.com/dgz2rwp09/image/upload/v1750759328/74751946173_upgo4n.png"
+        alt="شعار دار دز"
         width={1024} // Original width for aspect ratio calculation
         height={1024} // Original height for aspect ratio calculation
         className="h-12 w-auto"
