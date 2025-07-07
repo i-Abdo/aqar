7dd6159e0beb73764367cc522da97311bbea2a68
abdo kh <abdo0050607@gmail.com>
2025-07-07 19:15:37 +0000

    CLOUDINARY_CLOUD_NAME dgz2rwp09
    CLOUDINARY_API_KEY
    864762196268355
    
    CLO

---
+++ src/sentry.edge.config.ts
@@ -0,0 +1,14 @@
+
+// This file configures the Sentry Server SDK to send events from the server.
+// It is used by Sentry to instrument middleware running on Vercel's Edge Runtime.
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1.0,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
