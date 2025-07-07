824290f9f185885acaccf3bb883f216b4cd94432
abdo kh <abdo0050607@gmail.com>
2025-07-07 19:02:55 +0000

    https://abdokh.sentry.io/share/issue/dd7d2c95a56b4e7dbc8ebe5e57d625d3/

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -9,5 +9,5 @@ Sentry.init({
   tracesSampleRate: 1.0,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: true,
+  debug: false,
 });
