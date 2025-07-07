c39c5446afa5953476ec0904f67b2e4800f9eb4f
abdo kh <abdo0050607@gmail.com>
2025-07-07 16:24:28 +0000

    ما رأيك في هذا هل ينفع وهل هو مجاني ؟؟؟ سؤال فقط + ارني مذا افعل في sent

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 
+import { withSentryConfig } from "@sentry/nextjs";
 import type {NextConfig} from 'next';
 
 const cspHeader = `
@@ -86,4 +87,30 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+export default withSentryConfig(
+  nextConfig,
+  {
+    // For all available options, see:
+    // https://github.com/getsentry/sentry-webpack-plugin#options
+
+    // Suppresses source map uploading logs during build
+    silent: true,
+    org: process.env.SENTRY_ORG,
+    project: process.env.SENTRY_PROJECT,
+  },
+  {
+    // For all available options, see:
+    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+
+    // Hides source maps from generated client bundles
+    hideSourceMaps: true,
+
+    // Automatically tree-shake Sentry logger statements to reduce bundle size
+    disableLogger: true,
+
+    // Enables automatic instrumentation of Vercel Cron Monitors.
+    // See the following for more information:
+    // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/patching-behavior/
+    automaticVercelMonitors: true,
+  }
+);
